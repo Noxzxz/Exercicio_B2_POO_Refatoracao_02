605b116cf49bc1ec384b6df707575cc451e2dbd7
Icaro2703 <icarodias1978@gmail.com>
2026-04-01 13:34:03 -0300

    Add copyright notice and clean up comments in Main.java
    Added copyright notice to Main.java and removed TIP comments.

--- exercicioListObserverOCP-maven/src/main/java/org/example/app/Main.java
+++ exercicioListObserverOCP-maven/src/main/java/org/example/app/Main.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2026. Feito por Icaro Dias Marculino.
+ */
+
 package org.example.app;
 
 import org.example.dao.EstimativaJavaDao;
@@ -9,9 +13,6 @@ import org.example.service.CalculadoraProdutividadePleno;
 import org.example.service.CalculadoraProdutividadeSenior;
 
 import java.util.List;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 public class Main {
     static void main(String[] args) {
